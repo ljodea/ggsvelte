546f1a62e8d715ca00dc575113bbe4e5f8cfb3d8
ljodea <liam.j.odea@gmail.com>
2026-07-29 13:25:31 -0500

    fix(theme): remove duplicate ThemeCalc exports and test keys

--- packages/svelte/src/lib/index.ts
+++ packages/svelte/src/lib/index.ts
@@ -233,7 +233,6 @@ export { default as ThemeSolarized2dark } from "./theme/ThemeSolarized2dark.svel
 /** @lifecycle stable-intent */
 export { default as ThemeWsj } from "./theme/ThemeWsj.svelte";
 /** @lifecycle stable-intent */
-/** @lifecycle stable-intent */
 export { default as ThemeCalc } from "./theme/ThemeCalc.svelte";
 /** @lifecycle stable-intent */
 export { default as ThemeExcel } from "./theme/ThemeExcel.svelte";
